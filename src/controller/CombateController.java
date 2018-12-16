9375375ae10073f20d9a2ac673048a08451ca91c
Pedro Mota <pedromotafsa@hotmail.com>
2018-12-16 19:14:54 -0300

    metodo ataque terra

--- src/controller/CombateController.java
+++ src/controller/CombateController.java
@@ -23,6 +23,89 @@ public class CombateController {
 		}
 	}	
 	
+	public void combate(Jogador jogador, Tabuleiro mapa, Estado origem, Estado destino) {
+		boolean ret = false;
+		boolean podeAtacar = true;
+		if (!(origem.getDominante().equals(jogador)) || destino.getDominante().equals(jogador)) {				
+			System.out.println("Opcao de estado de origem ou destino invalidas.");			
+			podeAtacar = false;
+		} else if (!(origem.fazFronteiraCom(destino.getNome()))) {
+			System.out.println("Estados devem fazer fronteira!");			
+			podeAtacar = false;
+		} else if (origem.getQuantidade_de_Tropas() == 1) {
+			System.out.println("Eh necessario mais tropas para atacar!");
+			podeAtacar = false;
+		}		
+//			Entra aqui l�gica para ataque!
+		if (podeAtacar) {
+			int tropasAtacantes = setTropas(origem.getQuantidade_de_Tropas()-1);
+			int tropasDefensoras = setTropas(destino.getQuantidade_de_Tropas());
+			
+			ArrayList<Integer> dadosAtaque = new ArrayList<Integer>();					
+			ArrayList<Integer> dadosDefesa = new ArrayList<Integer>();
+					
+			for (int i = 0; i < tropasAtacantes;i++) {
+				Random rn = new Random();
+				dadosAtaque.add(rn.nextInt(6)+1);			
+			}
+			
+			for (int i = 0; i < tropasDefensoras;i++) {
+				Random rn = new Random();
+				dadosDefesa.add(rn.nextInt(6)+1);						
+			}
+			
+			Collections.sort(dadosAtaque,Collections.reverseOrder());
+			Collections.sort(dadosDefesa,Collections.reverseOrder());
+			
+			int combates = Math.min(dadosAtaque.size(), dadosDefesa.size());
+					
+			for (int i = 0; i < combates; i++) {
+				System.out.println("Luta " + (i+1) + ": Ataque: " + dadosAtaque.get(i) + " vs " + dadosDefesa.get(i) + " :Defesa");
+				if (dadosAtaque.get(i) <= dadosDefesa.get(i)) {
+					System.out.println("Defesa venceu a luta " + (i+1));					
+					origem.addTropas(-1);
+				} else {
+					System.out.println("Ataque venceu a luta " + (i+1));
+					destino.addTropas(-1);
+				}
+				System.out.println("------------------------------");
+			}
+			
+			if (destino.getQuantidade_de_Tropas() == 0) {
+				System.out.println("Jogador " + jogador.getCor() + " ganhou o territ�rio " + destino.getNome());
+				destino.getDominante().alteraTerritorio(-1);
+				destino.setDominante(jogador);
+				jogador.alteraTerritorio(1);
+				ret = true;
+				int move = 0;
+				if ((origem.getQuantidade_de_Tropas()-1) >= 3) {
+					move = 3;				
+				} else {
+					move = (origem.getQuantidade_de_Tropas()-1);
+				}			
+				boolean check = false;
+				do {
+					System.out.println("Quantas vai passar? (Entre 1 e " + move + ")");
+//					int escolha = scan.nextInt();
+					int escolha = 0; // receber da interface
+					
+					if (escolha >= 1 && escolha <= move) {
+						System.out.println("Movendo " + escolha + " de " + origem.getNome() + " para " + destino.getNome());
+						destino.addTropas(escolha);
+						origem.addTropas(-escolha);
+						
+						check = false;
+					} else {
+						System.out.println("Deve passar ao menos 1 tropa!");
+						check = true;
+					}			
+				} while (check);
+			}
+						    
+			mapa.printMapa();	
+		}
+	}
+	
 	public boolean combateTropas(Jogador jogador, Tabuleiro mapa) throws Exception {
 		boolean ret = false;
 		System.out.println("Modo Combate");
